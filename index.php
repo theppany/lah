b54ad4260ff2db9e380c40396216ae9e335d033f
theppany thienkhanh <theppany@gmail.com>
2021-08-16 16:41:12 +0700

    Update index.php
    kkkk

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ if(isset($_GET['locale'])){
 
 <html>
   <body>
-    <ul style="float:right;list-style-type: none;"><li style="float:left"><a style="text-decoration:none" href="/?locale=en"><?php ($_GET['locale']=='en')? echo 'EN': echo 'ອັງກິດ'; ?></a></l> | <li style="float:left"><a style="text-decoration:none" href="/?locale=la">LA</a></li></ul>
+    <ul style="float:right;list-style-type: none;"><li style="float:left"><a style="text-decoration:none" href="/?locale=en">EN</a></l> | <li style="float:left"><a style="text-decoration:none" href="/?locale=la">LA</a></li></ul>
     <img src="./demo.png"></img>
   </body>  
 </html>
