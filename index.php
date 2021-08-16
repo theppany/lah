74d198bc6df8accdf625c956a06c3cafd79100c7
theppany thienkhanh <theppany@gmail.com>
2021-08-16 16:31:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
   <body>
-    <ul style="float:right;"><li style="float:left"><a href="/?locale=en">EN</a></li><li style="float:left"><a href="/?locale=la">LA</a></li></ul>
+    <ul style="float:right;"><li style="float:left"><a style="text-decoration:none" href="/?locale=en">EN</a></li>&bnsp;&bnsp; | &bnsp;&bnsp;<li style="float:left"><a style="text-decoration:none" href="/?locale=la">LA</a></li></ul>
     <img src="./demo.png"></img>
   </body>  
 </html>
